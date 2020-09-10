8f8c3b59c7e1563e57200e2bec5d655001452a8c
Hirochon <heacet43@gmail.com>
2020-09-11 02:33:21 +0900

    [mod] make_mirror_dataを理想的な形に修正

--- research/make_mirror_data.py
+++ research/make_mirror_data.py
@@ -14,17 +14,17 @@ def make_elip_param_list(elip_params, dat_num):
 
     for dat_i in range(dat_num):
 
-        ellipse_nums.append(np.random.randint(elip_params['elip_num_min'], elip_params['elip_num_max']))    # 楕円の数
+        ellipse_nums.append(np.random.randint(elip_params["elip_num_min"], elip_params["elip_num_max"]))    # 楕円の数[vector]
 
-        init_elip_len_x_list = np.random.randint(elip_params['elip_len_x_min'], elip_params['elip_len_x_max'], size=ellipse_nums[dat_i])    # 楕円の長さ(横)
-        init_elip_len_y_list = np.random.randint(elip_params['elip_len_y_min'], elip_params['elip_len_y_max'], size=ellipse_nums[dat_i])    # 楕円の長さ(縦)
-        init_coord_x_list = np.random.randint(elip_params['coord_x_min'], elip_params['coord_x_max'], size=ellipse_nums[dat_i])     # 楕円の中心からのズレ(横)
-        init_coord_y_list = np.random.randint(elip_params['coord_y_min'], elip_params['coord_y_max'], size=ellipse_nums[dat_i])     # 楕円の中心からのズレ(縦)
-        init_theta_list = np.pi * np.random.rand(ellipse_nums[dat_i]) * 2 / elip_params['theta_rate']
+        init_elip_len_x_list = np.random.randint(elip_params["elip_len_x_min"], elip_params["elip_len_x_max"], size=ellipse_nums[dat_i])    # 楕円の長さ(横)[matrix]
+        init_elip_len_y_list = np.random.randint(elip_params["elip_len_y_min"], elip_params["elip_len_y_max"], size=ellipse_nums[dat_i])    # 楕円の長さ(縦)[matrix]
+        init_coord_x_list = np.random.randint(elip_params["coord_x_min"], elip_params["coord_x_max"], size=ellipse_nums[dat_i])     # 楕円の中心からのズレ(横)[matrix]
+        init_coord_y_list = np.random.randint(elip_params["coord_y_min"], elip_params["coord_y_max"], size=ellipse_nums[dat_i])     # 楕円の中心からのズレ(縦)[matrix]
+        init_theta_list = np.pi * np.random.rand(ellipse_nums[dat_i]) * 2 / elip_params["theta_rate"]   # 楕円の回転角[matrix]
 
         param_list.append(np.array([init_elip_len_x_list, init_elip_len_y_list, init_coord_x_list, init_coord_y_list, init_theta_list]))
         
-    return param_list, ellipse_nums, elip_params['axis_x'], elip_params['axis_y'], elip_params['nx'], elip_params['ny']
+    return param_list, ellipse_nums, elip_params["axis_x"], elip_params["axis_y"], elip_params["nx"], elip_params["ny"]
 
 
 def make_elip_spot_mirror(elip_len_x_list, elip_len_y_list, coord_x_list, coord_y_list, theta_list, axis_x, axis_y, ellipse_num, nx, ny):
@@ -42,32 +42,39 @@ def make_elip_spot_mirror(elip_len_x_list, elip_len_y_list, coord_x_list, coord_
                 
                 [X, Y] = np.dot([x - coord_x_list[k], y - coord_y_list[k]], rotate)     # 行列の積
 
-                x_formula = X**2 / elip_len_x_list[k]**2    # 楕円の方程式のx部分
-                y_formula = Y**2 / elip_len_y_list[k]**2    # 楕円の方程式のy部分
+                x_formula = X**2 / elip_len_x_list[k]**2    # 楕円の方程式のxとa部分
+                y_formula = Y**2 / elip_len_y_list[k]**2    # 楕円の方程式のyとb部分
 
                 if x_formula + y_formula <= 1:
                     elip_spot_mirror[j, i] += 1
 
-    xx = np.linspace(-0.5, 0.5, nx) * axis_x    # -0.5〜0.5間でnx個に分けて、axis_xでブロードキャスト
-    yy = np.linspace(-0.5, 0.5, ny) * axis_y    # -0.5〜0.5間でny個に分けて、axis_yでブロードキャスト
+    # 楕円作成後にnxとnyに合わせてスケールを変化させることを理想としている。
+    # 現在は未実装なので、np.linspace(-0.5, 0.5, nx) が np.linspace(-0.5, 0.5, axis_x)となってる。
+    # つまりnxとnyは引数として存在しているが、何もしていない。実装予定(記: 2020/9/11)。
+    xx = np.linspace(-0.5, 0.5, axis_x) * axis_x    # -0.5〜0.5間でaxis_x個に分けて、axis_xでブロードキャスト
+    yy = np.linspace(-0.5, 0.5, axis_y) * axis_y    # -0.5〜0.5間でaxis_y個に分けて、axis_yでブロードキャスト
     x, y = np.meshgrid(xx, yy, indexing="ij")   # 2次元配列としてxとyをそれぞれ用意
     
-    third_dim_elip_spot_mirror = {'m': 1, 'n': 1, 'x': x, 'y': y, 'z': elip_spot_mirror}
+    third_dim_elip_spot_mirror = {"x": x, "y": y, "z": elip_spot_mirror}
     
     return third_dim_elip_spot_mirror
 
 
 def _make_mirror_data(surface_params, is_train):
     if is_train:
-        elip_dat_num = surface_params['elip']['train_num']
+        elip_dat_num = surface_params["elip"]["train_num"]
     else:
-        elip_dat_num = surface_params['elip']['test_num']
+        elip_dat_num = surface_params["elip"]["test_num"]
     
-    elip_param_list, ellipse_nums, axis_x, axis_y, nx, ny = make_elip_param_list(surface_params['elip'], elip_dat_num)
+    elip_param_list, ellipse_nums, axis_x, axis_y, nx, ny = make_elip_param_list(surface_params["elip"], elip_dat_num)
 
     mirror_data = []
     for [elip_len_x_list, elip_len_y_list, coord_x_list, coord_y_list, theta_list], ellipse_num in tqdm(zip(elip_param_list, ellipse_nums), total=elip_dat_num):
-        mirror_data.append(make_elip_spot_mirror(elip_len_x_list, elip_len_y_list, coord_x_list, coord_y_list, theta_list, axis_x, axis_y, ellipse_num, nx, ny))
+        elip_spot_mirror = make_elip_spot_mirror(elip_len_x_list, elip_len_y_list, coord_x_list, coord_y_list, theta_list, axis_x, axis_y, ellipse_num, nx, ny)
+        elip_spot_mirror.update({"elip_len_x_list": elip_len_x_list, "elip_len_y_list": elip_len_y_list,
+                                 "coord_x_list": coord_x_list, "coord_y_list": coord_y_list, "theta_list": theta_list,
+                                 "axis_x": axis_x, "axis_y": axis_y, "ellipse_num": ellipse_num, "nx": nx, "ny": ny})
+        mirror_data.append(elip_spot_mirror)
 
     return mirror_data
 
@@ -83,7 +90,7 @@ def make_mirror_data(surface_params):
     print()
     print("Make test data!")
     print("out=", surface_params["pkl_surface_test"])
-    test_mirror_data = _make_mirror_data(surface_params["shape"], is_train=True)
+    test_mirror_data = _make_mirror_data(surface_params["shape"], is_train=False)
 
     return train_mirror_data, test_mirror_data
 
@@ -95,14 +102,14 @@ if __name__ == "__main__":
     with open(start_folder + name_json_surface_params, "r") as f:
         surface_params = json.load(f)
 
-    folder_name = make_dir(surface_params['dir_name'], is_time=True, pre_folder='result/')
+    folder_name = make_dir(surface_params["dir_name"], is_time=True, pre_folder="result/")
 
-    with open(folder_name + '/' + name_json_surface_params, "w") as f:
+    with open(folder_name + "/" + name_json_surface_params, "w") as f:
         json.dump(surface_params, f, indent=2)
 
     train_mirror_data, test_mirror_data = make_mirror_data(surface_params)
 
-    with open(folder_name + '/' + surface_params["pkl_surface_train"], "wb") as f:
+    with open(folder_name + "/" + surface_params["pkl_surface_train"], "wb") as f:
         pickle.dump(train_mirror_data, f)
-    with open(folder_name + '/' + surface_params["pkl_surface_test"], "wb") as f:
+    with open(folder_name + "/" + surface_params["pkl_surface_test"], "wb") as f:
         pickle.dump(test_mirror_data, f)
